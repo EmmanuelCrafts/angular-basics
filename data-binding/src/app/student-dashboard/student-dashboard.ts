6fdf3a62d975d805787bc46fc41e502482a59b07
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2026-06-04 10:17:38 +0300

    feat: add age increment functionality with maximum age validation

--- data-binding/src/app/student-dashboard/student-dashboard.ts
+++ data-binding/src/app/student-dashboard/student-dashboard.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { SlicePipe, UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-student-dashboard',
@@ -13,20 +13,30 @@ export class StudentDashboard {
   age: number = 20;
   major: string = 'Computer Science';
   isDisabled: boolean = true;
-  toggleText: string = "Enable Reset Button";
+  toggleText: string = 'Enable Reset Button';
+  isIncreaseAgeButtonDisabled: boolean = false;
+  errorMessage: string = '';
 
   // click counter
   counter: number = 0;
   increaseCounter() {
     this.counter += 1;
   }
-//  reset function
-resetName () {
-  this.studentName = "";
-}
+  //  reset function
+  resetName() {
+    this.studentName = '';
+  }
 
-toggleResetButton () {
-  this.isDisabled = !this.isDisabled;
-  this.toggleText = this.isDisabled ? "Enable Reset Button" : "Disable Reset Button";
-}
+  toggleResetButton() {
+    this.isDisabled = !this.isDisabled;
+    this.toggleText = this.isDisabled ? 'Enable Reset Button' : 'Disable Reset Button';
+  }
+
+  increaseAge() {
+    this.age += 1;
+    if (this.age === 25) {
+      this.isIncreaseAgeButtonDisabled = true;
+      this.errorMessage = 'Maximum student age reached.';
+    }
+  }
 }
